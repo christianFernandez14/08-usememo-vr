b568b7f2450ca7e99f13a78f75076c9d68b9879b
Christian Fernandez <christian14@gmail.com>
2024-05-05 17:59:51 -0400

    Pequeña app de tareas para el un abre boca del useMemo / algo de styles .129

--- src/components/Tareas.jsx
+++ src/components/Tareas.jsx
@@ -1,18 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 
 const Tareas = () => {
+
+  const [tareas, setTareas] = useState([])
+
+
+  const guardarTarea = e => {
+    e.preventDefault()
+    setTareas(tarea => [
+      ...tarea,
+      e.target.descripcion.value
+    ])
+
+  }
+
   return (
     <div className='tareas-container'>
       <h1>Mis tareas: </h1>
-      <form >
+      <form onSubmit={guardarTarea}>
         <input
-          name='tarea'
+          name='descripcion'
           type="text"
           placeholder='Nombre de la tarea'
         />
-        <input type="submit" value='guardar' />
+        <input
+          type="submit"
+          value='guardar'
+        />
       </form>
+      <hr />
+      <h3>Lista de tareas agregadas</h3>
+      <ol>
+        {
+          tareas.map(tarea => (
+            <li key={tarea}>
+              {tarea}
+            </li>
+          ))
+        }
+      </ol>
+
     </div>
   )
 }
