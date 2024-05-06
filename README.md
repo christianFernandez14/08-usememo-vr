3b869f7e75560a69d3a57289cbbe2cc10cb36917
Christian Fernandez <christian14@gmail.com>
2024-05-06 09:10:20 -0400

    Mas Ejemplos y la finalización de como se usa el useMemo y el memo como componente .131

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 Trabajando con useMemo
 
 El uso del hook useMemo en el componente Empleados es una buena práctica para optimizar el rendimiento y reducir el consumo de recursos. Sin embargo, es importante comprender su funcionamiento y utilizarlo de manera adecuada para obtener los mejores resultados; es lo que pudimos ver el primer ejercicio de practica del useMemo
+
+Su sintaxis const nombreFuncion = useMemo (() => funcionQueLlevaLaCarga()), [dependencia])
+
+Se utiliza para que no haya problema de rendimientos.
--- src/components/Tareas.jsx
+++ src/components/Tareas.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react'
+import React, { useMemo, useState } from 'react'
 
 
 const Tareas = () => {
 
   const [tareas, setTareas] = useState([])
+  const [contador, setContador] = useState(500)
 
 
   const guardarTarea = e => {
@@ -18,11 +19,32 @@ const Tareas = () => {
 
     // Filtrar las tareas para borrar la que no quiero
     let nuevas_tareas = tareas.filter((tarea, indice) => indice !== id)
-    
+
     // Guardar el nuevo Estado de tareas  
     setTareas(nuevas_tareas)
 
   }
+
+  const sumarAlContador = () => {
+    setContador(contador + 1)
+  }
+
+  const contadoresPasados = (acumulacion) => {
+
+    // La idea de esto es que acá estar la logica que desee, peticiones a otro sitios, no importa lo pesadas que sean, ya que esto dependera del la declaracion que hicimos abajo con useMemo y que dependia esclusivamente de contador
+
+    for (let i = 0; i <= acumulacion; i++) {
+
+      console.log('Ejecutando acumulación de contadores del pasado...')
+      console.log(`Se le sumó ${i} al contador con fecha 01-01-1991`)
+
+    }
+
+    return `Contador manual de tareas: ${acumulacion}`
+  }
+
+
+  const memoContadores = useMemo(() => (contadoresPasados(contador)), [contador])
 
   return (
     <div className='tareas-container'>
@@ -39,6 +61,10 @@ const Tareas = () => {
         />
       </form>
       <hr />
+
+      <h3>{memoContadores}</h3>
+      <button onClick={sumarAlContador}>Sumar</button>
+
       <h3>Lista de tareas agregadas</h3>
       <ol>
         {
